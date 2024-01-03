e4b078ce0c22fadd470bb42e2de398b822057230
User <kimbiggs1988@gmail.com>
2024-01-03 10:28:30 -0600

    Adding conditionals.json

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+let x = 4
+let y = 5
+
+if(y > x) {
+    console.log("y is greater than x!")
+
+}
+
+if(x > y) {
+    console.log("y is greater than x!")
+} else{
+    console.log("Nope, x is not greater than y.")
+}
